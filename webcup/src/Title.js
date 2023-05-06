63aa3052adc599e2f1967fcc03699e0fbd265401
noahlagrange <noah.lagrange@epitech.eu>
2023-05-06 16:38:46 +0400

    new_form

--- webcup/src/Title.js
+++ webcup/src/Title.js
@@ -3,9 +3,13 @@ import React from "react";
 
 function Title() {
     return (
-    <div>
-        <h1> Les jones AI </h1>
-        <h2> Don't let your dream be dream </h2>
+    <div className="Title">
+        <title>LucidOracle.com</title>
+        <h1>text.txt</h1>
+        <h2> truc bleu</h2>
+        <h1>blabla</h1>
+
+        <h3>"quote"</h3>
     </div>
     );
   }
