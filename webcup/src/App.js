7471a5066e3759db8ab1e816260255ee1c2ccd3e
noahlagrange <noah.lagrange@epitech.eu>
2023-05-06 15:02:24 +0400

    title

--- webcup/src/App.js
+++ webcup/src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
-import Home from "./Components/home/Home"
+import Home from "./Components/home/Home";
+import Head from "./Title";
 
-import "./Components/navbar/Navbar.css"
+import "./Components/navbar/Navbar.css";
 import './App.css';
 
 function App() {
   return (
+    <div>
     <Home />
-  )
+    <Head />
+    </div>
+    )
 }
 
 export default App;
---
+++ webcup/src/Title.js
@@ -0,0 +1,14 @@
+
+import React from "react";
+
+function Title() {
+    return (
+    <div>
+        <h1> Les jones AI </h1>
+        <h2> Don't let your dream be dream </h2>
+    </div>
+    );
+  }
+
+
+export default Title;
