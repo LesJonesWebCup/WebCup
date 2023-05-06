e437143d10391f43ee3d933a4fc274d02d9e5204
deadpanda <clement.lagier@epitech.eu>
2023-05-06 11:04:42 +0400

    modified the import of the App.js for the router part

--- webcup/src/App.js
+++ webcup/src/App.js
@@ -1,4 +1,7 @@
-import { Routes, Route } from "react-router-dom";
+import {
+  HashRouter as Routes,
+  Route,
+  } from 'react-router-dom';
 import React from "react";
 import Home from "./Components/home/Home"
 import About from "./Components/about/About"
